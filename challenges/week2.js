189f2753525324e31c9d5a2b298836731d1f1297
Mirabela Dan <mirabeladan@live.de>
2019-11-12 20:09:24 +0000

    added week 2 answers

--- challenges/week2.js
+++ challenges/week2.js
@@ -1,26 +1,43 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.city === "Manchester") {
+    return true;
+  }
+  return false;
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  if (people % 40 === 0) {
+    return Math.floor(people / 40);
+  }
+  else {
+    return Math.floor(people / 40) + 1;
+  }
 }
 
 function countSheep(arr) {
+  let count = 0;
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] === "sheep") {
+      count +=1;
+    }
+  }
+  return count;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if (person.address.postCode[1] && person.address.city === "Manchester") {
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
