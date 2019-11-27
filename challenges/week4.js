b6d10bd9f08a980f0dd4511c420ad3af2ed3e390
Mirabela Dan <mirabeladan@live.de>
2019-11-27 17:18:04 +0000

    first four challenges

--- challenges/week4.js
+++ challenges/week4.js
@@ -1,22 +1,46 @@
 function findSmallNums(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  function greaterOne(value) {
+    return value < 1;
+  }
+
+  var filtered = nums.filter(greaterOne);
+  return (filtered);
 }
 
 function findNamesBeginningWith(names, char) {
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+  let namesArr = [];
+  for (let i = 0; i < names.length; i++) {
+    if (names[i][0] === char) {
+      namesArr.push(names[i]);
+    }
+  }
+  return namesArr;
 }
 
 function findVerbs(words) {
+  let verbArr = [];
   if (!words) throw new Error("words is required");
-  // Your code here
+for (let i =0; i<words.length; i++) {
+  if (words[i].startsWith("to ") === true) {
+verbArr.push(words[i]);
+}
+}
+return verbArr;
 }
 
 function getIntegers(nums) {
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  let integerArr = [];
+for (let i=0; i<nums.length; i++) {
+  if (Number.isInteger(nums[i]) === true) {
+    integerArr.push(nums[i]);
+  }
+}
+return integerArr;
 }
 
 function getCities(users) {
