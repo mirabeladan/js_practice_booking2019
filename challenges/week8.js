df736c1f02605edee949900432ea0ebb5e7bdfe1
Mirabela Dan <mirabeladan@live.de>
2020-01-12 23:01:26 +0000

    added final two and modified 1 to pass final test

--- challenges/week8.js
+++ challenges/week8.js
@@ -1,17 +1,13 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-let p = nums.indexOf(n);
-if (p !== -1) {
-return nums[p +1];
-}
-if (p === -1) {
-  return null
-}
-if (p === nums.legth - 1) {
-  return null
-}
-}
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === n) {
+      return nums[i + 1];
+    }
+  }
+  return null;
+};
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
@@ -60,12 +56,22 @@ const arrShift = arr => {
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
   if (searchTerm === undefined) throw new Error("searchTerm is required");
-  // Your code here!
+  return Object.values(haystack).some(val => {
+    if (typeof val === 'string') {
+      return val.toLowerCase().includes(searchTerm.toLowerCase())
+    }
+  })
 };
 
 const getWordFrequencies = str => {
   if (str === undefined) throw new Error("str is required");
-  // Your code here!
+
+  let frequencies = str.replace(/[^\w\s]/gi, '').toLowerCase().split(' ')
+  const obj = {};
+  frequencies.forEach(n => {
+  obj[n] = obj[n] ? obj[n] + 1 : 1;
+  });
+  return obj
 };
 
 module.exports = {
