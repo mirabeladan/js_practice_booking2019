8f40c0714d3b10ad9d19a6b9d679e2e07d8ef895
Mirabela Dan <mirabeladan@live.de>
2020-01-12 21:53:22 +0000

    solved challenges except for 2 and final 2

--- challenges/week8.js
+++ challenges/week8.js
@@ -1,8 +1,17 @@
 const findNextNumber = (nums, n) => {
   if (nums === undefined) throw new Error("nums is required");
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
-};
+let p = nums.indexOf(n);
+if (p !== -1) {
+return nums[p +1];
+}
+if (p === -1) {
+  return null
+}
+if (p === nums.legth - 1) {
+  return null
+}
+}
 
 const count1sand0s = str => {
   if (str === undefined) throw new Error("str is required");
@@ -11,18 +20,26 @@ const count1sand0s = str => {
 
 const reverseNumber = n => {
   if (n === undefined) throw new Error("n is required");
-  // Your code here!
+   return Number(n.toString().split("").reverse().join(""));
 };
 
 const sumArrays = arrs => {
   if (arrs === undefined) throw new Error("arrs is required");
-  // Your code here!
-};
+return arrs.toString().split(',').map(Number).reduce((a, b) => a + b, 0);
+}
+;
 
 const arrShift = arr => {
-  if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-};
+  let temp = 0;
+  if (arr.length < 2){
+    return arr;
+  }else{
+    temp = arr[0];
+    arr[0] = arr[arr.length-1];
+    arr[arr.length-1] = temp;
+  }
+  return arr;
+}
 
 const findNeedle = (haystack, searchTerm) => {
   if (haystack === undefined) throw new Error("haystack is required");
